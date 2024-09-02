59ac906cf4d37cfd8303b79d72680339dcb936d3
Mauricio Argumedo <mauargudev@gmail.com>
2024-09-02 11:41:10 -0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Mauricio Argumedo on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
